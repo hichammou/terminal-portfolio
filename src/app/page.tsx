9df8223e56cc98371fa2fcca0c41582482880722
hichammou <moulilihicham8@gmail.com>
2025-10-17 16:24:45 +0100

    src/components/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,100 @@
-import Image from "next/image";
+"use client";
+
+import History from "@/components/history";
+import HistoryContextProvider from "@/context/HistoryContext";
+import { linux } from "@/lib/ascii-logos";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const name = String.raw`    __  ______________  _____    __  ___   __  _______  __  ____    ______    ____
+   / / / /  _/ ____/ / / /   |  /  |/  /  /  |/  / __ \/ / / / /   /  _/ /   /  _/
+  / /_/ // // /   / /_/ / /| | / /|_/ /  / /|_/ / / / / / / / /    / // /    / /  
+ / __  // // /___/ __  / ___ |/ /  / /  / /  / / /_/ / /_/ / /____/ // /____/ /   
+/_/ /_/___/\____/_/ /_/_/  |_/_/  /_/  /_/  /_/\____/\____/_____/___/_____/___/   
+                                                                                  `;
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div className="min-h-screen text-sm px-4 py-7 space-y-4 text-foreground-text dark:text-gray">
+      <HistoryContextProvider>
+        {/* The first terminal input */}
+        {/* <Input /> */}
+        <div className="flex gap-20">
+          <div>
+            <pre className="font-mono whitespace-pre text-sm leading-tight bg-gradient-to-l from-green to-red px-3 bg-clip-text text-transparent">
+              {name}
+            </pre>
+
+            <div className="mt-4 space-y-2">
+              <p>Welcome to my terminal protfolio. (Version 1.0.0)</p>
+              <p>---------</p>
+              <p>
+                This Project's source code can be found in this{" "}
+                <span className="text-pink underline decoration-dashed hover:decoration-solid transition-all">
+                  Github Repo
+                </span>
+              </p>
+              <p>---------</p>
+              <p>
+                run '<span className="text-pink">help</span>' to see the list of
+                available commands.
+              </p>
+              <p>---------</p>
+              <div>
+                <pre className="text-blue">
+                  {"ctrl + l".padEnd(15, " ")} {"-->"} clear terminal output
+                </pre>
+                <pre className="text-blue">
+                  {"tab".padEnd(15, " ")} {"-->"} autocomplete command
+                </pre>
+                <pre className="text-blue">
+                  {"↑ & ↓".padEnd(15, " ")} {"-->"} navigate command history
+                </pre>
+              </div>
+            </div>
+          </div>
+
+          <pre className="font-mono whitespace-pre text-sm leading-tight bg-gradient-to-r from-green to-red px-3 bg-clip-text text-transparent">
+            {linux}
+          </pre>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <History />
+        {/* <Help /> */}
+      </HistoryContextProvider>
     </div>
   );
 }
+
+/*
+------ dark -----
+#1E1E2E // background
+
+// red
+#E06C75
+#F38BA8
+
+// gray
+#BAC2DE
+#A6ADC8
+
+// green
+#7ED88B
+#A6E3A1
+
+// yellow
+#EBD391
+#F9E2AF
+
+// blue
+#89B4FA
+#74A8FC
+
+// pink
+#F5C2E7
+#F2AEDE
+
+// cyan
+#94E2D5
+#6BD7CA
+
+------ light -----
+#EFF1F5 // background
+*/
