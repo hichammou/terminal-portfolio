f4c88bdbe6d943b1a5ba247b9d914298f334b6e0
hichammou <moulilihicham8@gmail.com>
2025-10-18 17:04:19 +0100

    fix: render only the non empty histories when entring `history` command

--- src/components/commands/history.tsx
+++ src/components/commands/history.tsx
@@ -8,11 +8,14 @@ function History() {
   const [renderHistory] = useState(history);
   return (
     <ul>
-      {renderHistory.map((item, index) => (
-        <li key={`${index}-${item}`} className="mb-1">
-          <span className="text-pink">$</span> {item}
-        </li>
-      ))}
+      {renderHistory.map(
+        (item, index) =>
+          item && (
+            <li key={`${index}-${item}`} className="mb-1">
+              <span className="text-pink">$</span> {item}
+            </li>
+          )
+      )}
     </ul>
   );
 }
